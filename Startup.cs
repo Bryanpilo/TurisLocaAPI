882a26d27fe518922b40adae7ebeae026cb6244f
Bryan Espinoza <Bryan.Espinoza@cr.ey.com>
2019-09-04 09:24:58 -0600

    Generation of handle mehods for production mode

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +23,7 @@ using TurisLocAPI.API.Business.Implementation;
 using TurisLocAPI.API.Business.Interface;
 using TurisLocAPI.API.Data;
 using TurisLocAPI.API.DTO.User;
+using TurisLocAPI.API.Helper;
 using TurisLocAPI.API.Models;
 using TurisLocAPI.API.Repository;
 using TurisLocAPI.API.Repository.Implementation;
@@ -45,20 +49,22 @@ namespace TurisLocAPI.API
             services.AddCors();
 
             //  services.AddAutoMapper(typeof(UserBL).Assembly);
-            
+
             ConfigureServiceInstances(services);
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(Options=>{
-                    Options.TokenValidationParameters= new TokenValidationParameters{
-                        ValidateIssuerSigningKey= true,
-                        IssuerSigningKey= new SymmetricSecurityKey(Encoding.ASCII
+                .AddJwtBearer(Options =>
+                {
+                    Options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
                             .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
-                        ValidateIssuer= false,
-                        ValidateAudience= false
+                        ValidateIssuer = false,
+                        ValidateAudience = false
                     };
                 });
-            
+
         }
 
 
@@ -69,7 +75,7 @@ namespace TurisLocAPI.API
 
             // services.AddScoped<IFacade, Facade>();
             services.AddScoped<IUserBL, UserBL>();
-            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -81,6 +87,24 @@ namespace TurisLocAPI.API
             }
             else
             {
+
+                //With this extension, we are able to handle errors when we want to use the production instance 
+                app.UseExceptionHandler(builder =>
+                {
+                    builder.Run(async context =>
+                    {
+                        //Generate a helper who apply it a formar and add headers parameters
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+
+                        if (error != null)
+                        {
+                            context.Response.AddAplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
                 // app.UseHsts();
             }
 
